0cec4f7c62318b4f6ec300100add53af04921ceb
otejeda <otejeda.ny@gmail.com>
2017-12-05 18:26:24 -0500

    Add files via upload

---
+++ My-Online-Library/inc/data.php
@@ -0,0 +1,172 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Harry Potter and the Deathly Hallows",
+	"img" => "img/media/Hp7-389x499.jpg",
+    "genre" => "Fantasy",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "J. K. Rowling"
+    ],
+    "publisher" => "Bloomsbury Publishing",
+    "isbn" => '0-545-01022-5'
+];
+$catalog[102] = [
+	"title" => "Don Quixote",
+"img" => "img/media/Don-quixote.jpg",
+	"genre" => "Novel",
+	"format" => "Print",
+	"year" => 1605,
+	"category" => "Books",
+	"authors" => [
+			"Miguel de Cervantes Saavedra"
+	],
+	"publisher" => "Francisco de Robles",
+	"isbn" => '978-0060934347'
+];
+$catalog[103] = [
+    "title" => "The Witch of Portobello",
+    "img" => "img/media/The-witch-of-Portobello.jpg",
+    "genre" => "Novel",
+    "format" => "Hardcover",
+    "year" => 2006,
+    "category" => "Books",
+    "authors" => [
+        "Paulo Coelho"
+    ],
+    "publisher" => "Harper Collins Publishers",
+    "isbn" => '978-0-06-133880-9'
+];
+$catalog[104] = [
+    "title" => "One Hundred Years of Solitude",
+    "img" => "img/media/One-hundred-years-of-Solitude.jpg",
+    "genre" => "Magical Realism, Novel",
+    "format" => "Hardcover",
+    "year" => 1967,
+    "category" => "Books",
+    "authors" => [
+        "Gabriel García Márquez"
+    ],
+    "publisher" => "Harper & Row (US)",
+    "isbn" => '9789631420494'
+];
+//Movies
+$catalog[201] = [
+	"title" => "IT",
+	"img" => "img/media/IT.jpg",
+	"genre" => "Thriller, Drama",
+	"format" => "DVD",
+	"year" => 1994,
+	"category" => "Movies",
+	"director" => "Robert Zemeckis",
+	"writers" => [
+			"Winston Groom",
+			"Eric Roth"
+	],
+	"stars" => [
+			"Tom Hanks",
+			"Rebecca Williams",
+			"Sally Field",
+			"Michael Conner Humphreys"
+	]
+];
+$catalog[202] = [
+	"title" => "Kingsman: The Golden Circle",
+	"img" => "img/media/Kingsman.jpg",
+	"genre" => "Action, Adventure",
+	"format" => "DVD",
+	"year" => 2017,
+	"category" => "Movies",
+	"director" => "Matthew Vaughn",
+	"writers" => [
+			"Matthew Vaughn",
+			"Jane Goldman"
+	],
+	"stars" => [
+			"Taron Egerton",
+			"Channing Tatum",
+			"Colin Firth",
+			"Halle Berry"
+	]
+];
+$catalog[203] = [
+    "title" => "Alien: Covenant",
+    "img" => "img/media/Alien-Covenant.jpg",
+    "genre" => "Horror, Sci-Fi, Thriller",
+    "format" => "Blu-ray",
+    "year" => 2017,
+    "category" => "Movies",
+    "director" => "Ridley Scott",
+    "writers" => [
+			"John Logan",
+			"Dante Harper",
+			"Jack Paglen",
+			"Michael Green"
+    ],
+    "stars" => [
+        "Michael Fassbender",
+        "Katherine Waterston",
+        "Danny McBride",
+        "Billy Crudup",
+				"Demián Bichir",
+				"James Franco"
+    ]
+];
+$catalog[204] = [
+    "title" => "Star Wars: The Last Jedi",
+    "img" => "img/media/The-last-Jedi.jpg",
+    "genre" => "Action, Adventure, Fantasy",
+    "format" => "DVD",
+    "year" => 2017,
+    "category" => "Movies",
+    "director" => "Rian Johnson",
+    "writers" => [
+        "Rian Johnson",
+				"George Lucas"
+    ],
+    "stars" => [
+        "Daisy Ridley",
+        "John Boyega",
+        "Mark Hamill"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "These Days",
+    "img" => "img/media/Bon-Jovi-These-Days.jpg",
+    "genre" => "Rock music, Hard rock, Alternative rock, Pop rock",
+    "format" => "CD",
+    "year" => 1995,
+    "category" => "Music",
+    "artist" => "Bon Jovi"
+];
+$catalog[302] = [
+    "title" => "Night Visions",
+    "img" => "img/media/imagine-dragons.jpg",
+    "genre" => "Rock music, Alternative rock, Pop music, Indie rock, Electropop, Pop rock",
+    "format" => "MP3",
+    "year" => 2012,
+    "category" => "Music",
+    "artist" => "Imagine Dragons"
+];
+$catalog[303] = [
+    "title" => "2014 Forest Hills Drive",
+    "img" => "img/media/2014-Forest-Hills-drive.jpg",
+    "genre" => "Hip-Hop, Rap",
+    "format" => "MP3, CD",
+    "year" => 2014,
+    "category" => "Music",
+    "artist" => "J. Cole"
+];
+$catalog[304] = [
+    "title" => "Damn.",
+    "img" => "img/media/Kendrick_Lamar_Damn.jpg",
+    "genre" => "Hip-Hop, Rap",
+    "format" => "MP3",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Kendrick Lamar"
+];
